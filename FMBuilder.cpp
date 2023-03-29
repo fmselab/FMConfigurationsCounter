bc0d0e73e811ac261121be795870e6c0d3b889ed
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2023-03-29 21:16:08 +0200

    Update FMBuilder.cpp

--- FMBuilder.cpp
+++ FMBuilder.cpp
@@ -6,8 +6,6 @@ using namespace std;
 #define IGNORE_HIDDEN_MAIN false
 
 int main(int argc, char **argv) {
-	if (Util::getProductCountFromFile("examples/violetModel.xml", IGNORE_HIDDEN_MAIN) != 186)
-		cerr << "Error for violetModel" << endl;
-
+	Util::getProductCountFromFile("examples/violetModel.xml", IGNORE_HIDDEN_MAIN);
 	return 0;
 }
