4655ee479962e8ba871de33dafa8ffcf6a7ee015
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2023-03-30 12:53:00 +0200

    Update model

--- FMBuilder.cpp
+++ FMBuilder.cpp
@@ -12,6 +12,6 @@ int main(int argc, char **argv) {
 	//				   Waterloo
 
 	Util::SORT_CONSTRAINTS = true;
-	Util::getProductCountFromFile("examples/linuxModel.xml", IGNORE_HIDDEN_MAIN);
+	Util::getProductCountFromFile("examples/gplAheadModel.xml", IGNORE_HIDDEN_MAIN);
 	return 0;
 }
