8d0dcf0c439fa3126ea77d515f5ee3ae2e5978d4
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2024-06-11 11:10:31 +0200

    Update

--- Other/benchmarks/fmgenerator.counter/dist/CountWithBDDs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT_FILE="results_with_bdds.csv"
-
-for file in $(ls ../../../examples/SPLC/); do
-	timeout 86400s java -Xmx8000m -jar ./Counter.jar -m ../../../examples/SPLC/$file -o $OUTPUT_FILE | echo -e "../../../examples/SPLC/${file};timeout;timeout;timeout" >> $OUTPUT_FILE
-done
